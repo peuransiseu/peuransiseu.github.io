28a2ee7e5b7891323bfa38320dfdc30686d120a5
peuransiseu <90952718+peuransiseu@users.noreply.github.com>
2025-08-31 20:29:33 +0800

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/peuransiseu.github.io/', 
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+  },
 })
